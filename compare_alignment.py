cc8c3bee48ea640f074bbafc92d76fe43807aa52
kunyanglu <lualicia88@gmail.com>
2022-04-02 23:00:18 -0700

    delete empty line

--- compare_alignment.py
+++ compare_alignment.py
@@ -1,21 +1,59 @@
 import re
+import numpy as np
 import pickle
+from sklearn.metrics import rand_score
+from categorize_files import categorize_files
+import os
 
-def format_clusters(annotation_file, communities):
+def get_thres_randscore_pairs(annotation_file, path, folder_id):
     
-    cluster_category_dict = get_clusters_from_annotation(annotation_file)
-    formated_clusters_grd = []
-    formated_clusters_alg = []
+    rand_scores = []
+    thresholds = [0.8 + i*0.01 for i in range(11)]
+    num_files = len(os.listdir(f'{path}/{folder_id}'))
+    
+    clusters_grd = get_clusters_from_annotation(annotation_file)
+    labels_grd = format_clusters(clusters_grd, num_files, 'grd')
+    print(f'labels_grd: {labels_grd}')
+
+    for threshold in thresholds:
         
-    for _, clusters in cluster_category_dict.items():
-        formated_clusters_grd.append(clusters)
-    
-    for community in communities:
-        community = [c.split('-')[-1].split('.')[0][-2:] for c in sorted(community)]
-        formated_clusters_alg.append(community) 
-    
-    return formated_clusters_grd, formated_clusters_alg
+        clusters_alg = categorize_files(path, folder_id, threshold)
+        labels_alg = format_clusters(clusters_alg, num_files, 'alg')
+        print(f'labels_alg: {labels_alg}')
 
+        rand_scores.append(rand_score(labels_grd, labels_alg))
+        print(f'rand_scores for threhold={threshold}: {rand_scores}')
+        break
+
+    return list(zip(thresholds, rand_scores))
+
+def format_clusters(clusters, num_files, typ):
+    
+    if typ == 'grd':
+        formated_clusters_grd = [] 
+        labels_grd = np.zeros(num_files+1)
+
+        for _, cls in clusters.items():
+            formated_clusters_grd.append(cls)
+
+        for i, cluster in enumerate(formated_clusters_grd):
+            for file_id in cluster: labels_grd[int(file_id)] = i + 1
+        
+        return labels_grd
+    
+    elif typ == 'alg':
+        formated_clusters_alg = []
+        labels_alg = np.zeros(num_files+1)
+
+        for cluster in clusters:
+            cls = [c.split('-')[-1].split('.')[0][-2:] for c in sorted(cluster)]
+            formated_clusters_alg.append(cls)
+
+        for i, cluster in enumerate(formated_clusters_alg):
+            for file_id in cluster: labels_alg[int(file_id)] = i + 1
+
+        return labels_alg
+    
 def get_clusters_from_annotation(annotation_file):
 
     f = open(annotation_file, 'r')
@@ -67,6 +105,8 @@ if __name__ == "__main__":
     annotation_file = 'sam-clusters.txt'
     f = open('sample_communities.pkl', 'rb')
     communities = pickle.load(f)
-    f.close()
-    formated_clusters = format_clusters(annotation_file, communities)
-    print(formated_clusters)
+    path = 'dcm_data'
+    folder_id = '22444_20180404'
+
+    get_thres_randscore_pairs(annotation_file, path, folder_id)
+
